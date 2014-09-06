93d30a1a429d67d1ab14fadcac6cf016e1604518
ggokalp <gabriel@gokalp.be>
2014-09-06 19:35:14 +0200

    Add Client detail devis and insert DB

---
+++ SQL FILES/DevisDetail.sql
@@ -0,0 +1,10 @@
+DROP TABLE devisdetail;
+
+CREATE TABLE devisdetail (
+	nopiece varchar(100), 
+	article varchar(100),
+	descritpion varchar(5000),
+	prixht double,
+	quantite integer,
+	prixtotht double
+);
