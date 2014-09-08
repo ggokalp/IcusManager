cc291ba8a8787d3b2ddf7e68bb74c14f1c8c7396
ggokalp <gabriel@gokalp.be>
2014-09-08 16:14:04 +0200

    Add SQL INSERT Article and correct CREATE

--- SQL FILES/ArticleCompose.sql
+++ SQL FILES/ArticleCompose.sql
@@ -1,7 +1,9 @@
 drop table articlecompose;
 
 CREATE TABLE articlecompose (
-  idartcompose int NOT NULL,
-  idart int NOT NULL,
+  idmaster int NOT NULL,
+  idslave int NOT NULL,
+  codemaster varchar(30) NOT NULL,
+  codeslave varchar(30) NOT NULL,
   quantite int
 );
